8259722e3cf572fe0b0990d1760256a211cfadbb
19522059 <19522059@gm.uit.edu.vn>
2023-12-26 13:33:17 +0700

    Complete show product by catagory screen

--- lib/screens/home/components/controller_and_accessory.dart
+++ lib/screens/home/components/controller_and_accessory.dart
@@ -1,4 +1,8 @@
 import 'package:e_console_store/constants/constants.dart';
+import 'package:e_console_store/models/product_model.dart';
+import 'package:e_console_store/models/products.dart';
+import 'package:e_console_store/screens/details/details_screen.dart';
+import 'package:e_console_store/screens/favorite/components/product_card.dart';
 import 'package:e_console_store/screens/home/components/section_title.dart';
 import 'package:e_console_store/screens/product/controllerandaccessory_screen.dart';
 import 'package:flutter/material.dart';
@@ -23,8 +27,51 @@ class ControllerAndAccessory extends StatelessWidget {
             },
           ),
         ),
-        //logic here
+        FutureBuilder<List<Product>>(
+          future: Products.getProductsCnA(),
+          builder: (context, snapshot) {
+            final List<Product>? examQuestions = snapshot.data;
+
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              default:
+                if (snapshot.hasError)
+                  return Center(child: Text(snapshot.error.toString()));
+                else if (examQuestions != null)
+                  return buildproduct(examQuestions);
+                else
+                  return Text("null");
+            }
+          },
+        ),
       ],
     );
+  }
+
+  Widget buildproduct(List<Product> list) {
+    return SizedBox(
+        height: 180,
+        width: double.infinity,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: list.length,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.only(right: defaultPadding),
+            child: ProductCard(
+              title: list[index].name,
+              image: list[index].image[0],
+              price: list[index].price,
+              bgColor: bgColor,
+              press: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(product: list[index]),
+                    ));
+              },
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/product/new_screen.dart
+++ lib/screens/product/new_screen.dart
@@ -1,5 +1,9 @@
 import 'package:e_console_store/constants/constants.dart';
+import 'package:e_console_store/models/product_model.dart';
+import 'package:e_console_store/models/products.dart';
 import 'package:e_console_store/screens/cart/cart_screen.dart';
+import 'package:e_console_store/screens/details/details_screen.dart';
+import 'package:e_console_store/screens/favorite/components/product_card.dart';
 import 'package:e_console_store/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -54,8 +58,54 @@ class _NewScreenState extends State<NewScreen> {
         ),
         body: Column(
           children: [
-            //logic here
+            FutureBuilder<List<Product>>(
+              future: Products.getProductsNew(),
+              builder: (context, snapshot) {
+                final List<Product>? examQuestions = snapshot.data;
+
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Center(child: CircularProgressIndicator());
+                  default:
+                    if (snapshot.hasError)
+                      return Center(child: Text(snapshot.error.toString()));
+                    else if (examQuestions != null)
+                      return buildproduct(examQuestions);
+                    else
+                      return Text("null");
+                }
+              },
+            ),
           ],
         ));
+  }
+
+  Widget buildproduct(List<Product> list) {
+    return Expanded(
+        child: Container(
+            decoration: BoxDecoration(color: barColor),
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(
+                list.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.all(defaultPadding / 2),
+                  child: ProductCard(
+                    title: list[index].name,
+                    image: list[index].image[0],
+                    price: list[index].price,
+                    bgColor: bgColor,
+                    press: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                DetailsScreen(product: list[index]),
+                          ));
+                    },
+                  ),
+                ),
+              ),
+            )));
   }
 }
